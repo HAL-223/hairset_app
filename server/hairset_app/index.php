34e4fce628b44ca262d5cead2b7879d1a52d5802
HAL-223 <anoble.ldy8@gmail.com>
2020-05-26 22:53:07 +0900

    削除機能追加

--- server/hairset_app/index.php
+++ server/hairset_app/index.php
@@ -6,6 +6,24 @@ session_start();
 
 $dbh = connectDb();
 
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  $keyword = $_GET['keyword'];
+
+  if ($keyword == '') {
+    $sql = "select * from styles";
+    $stmt = $dbh->prepare($sql);
+  } else {
+    $sql = "select * from styles where body like :keyword";
+    $stmt = $dbh->prepare($sql);
+    $keyword_param = '%' . $keyword . '%';
+    $stmt->bindParam(":keyword", $keyword_param);
+  }
+  $stmt->execute();
+
+  $styles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+// stylesの取得
 $sql = <<<SQL
 SELECT
   s.*,
@@ -24,13 +42,6 @@ ON
 ORDER BY
   s.created_at desc
 SQL;
-
-// $sql_where = " where s.category_id = :category_id";
-
-
-// $sql_order = " order by s.created_at desc";
-// // SQL結合
-// $sql = $sql . $sql_where . $sql_order;
 
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
@@ -54,7 +65,7 @@ $styles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <body>
   <div class="flex-col-area">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3">
       <a href="http://localhost/index.php" class="navbar-brand">Hair set style</a>
       <div class="collapse navbar-collapse" id="navbarToggle">
         <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
@@ -76,16 +87,28 @@ $styles = $stmt->fetchAll(PDO::FETCH_ASSOC);
         </ul>
       </div>
     </nav>
+
+    <div class="search pr-3 my-3">
+      <form action="" method="get">
+        <input type="text" name="keyword" placeholder="SEARCH">
+        <input type="submit" class="btn btn-dark" value="検索">
+      </form>
+      <!-- <form class="form-inline my-3">
+        <input class="form-control mr-sm-2" type="search" name="keyword" placeholder="SEARCH" aria-label="Search">
+        <button type="submit" class="btn btn-dark">検索</button>
+      </form> -->
+    </div>
     <div class="container">
       <div class="row">
         <div class="col-sm-10 col-md-10 col-lg-10 mx-auto">
           <div class="row">
             <?php foreach ($styles as $style) : ?>
-              <div class="col-md-5">
+              <div class="col-md-4">
                 <div class="article">
                   <a href="show.php?id=<?php echo h($style['id']) ?>"><img src="<?php echo h('style_img/' . $style['picture']); ?>" alt=""></a>
                   <p>☆:<?php echo h($style['user_name']); ?></p>
                   <p>投稿日:<?php echo h($style['created_at']); ?></p>
+                  <p><?php echo h($style['body']); ?></p>
                 </div>
                 <hr>
               </div>
